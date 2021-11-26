f280de09e6aefeabccf409cddac510d995d75116
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-11-26 12:55:13 +0530

    Update searchInsert.js
    Another solution

--- searchInsert.js
+++ searchInsert.js
@@ -48,3 +48,20 @@ let i=0;
         }
     }
      return i;
+/**
+ let left = 0;
+    let right = nums.length - 1;
+    let middle;
+    
+    while(left <= right){
+        middle = Math.floor((left + right) / 2);
+        if(nums[middle] === target) return middle;
+        else if(nums[middle] < target){
+            left = middle + 1;
+        } else{
+            right = middle - 1;
+        }
+    }
+    
+    return left;
+*/
