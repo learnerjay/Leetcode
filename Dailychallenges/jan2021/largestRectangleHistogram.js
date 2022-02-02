ad1b1abdd6c55ec3a81f2c44162c2e097541c5d6
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2022-02-02 23:04:41 +0530

    Add files via upload

---
+++ Dailychallenges/jan2021/largestRectangleHistogram.js
@@ -0,0 +1,76 @@
+/*
+84. Largest Rectangle in Histogram
+Hard
+
+8732
+
+132
+
+Add to List
+
+Share
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+Accepted
+460,166
+Submissions
+1,149,981
+*/
+Solution
+84. Largest Rectangle in Histogram
+Hard
+
+8732
+
+132
+
+Add to List
+
+Share
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+Accepted
+460,166
+Submissions
+1,149,981
+https://leetcode.com/submissions/detail/630305277/
